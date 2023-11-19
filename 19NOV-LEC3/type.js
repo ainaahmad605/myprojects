4482ada79a3e5fa8c0c9e8c6169b59554e754527
Aina Ahmad <ainaahmad605@gmail.com>
2023-11-19 15:13:16 +0500

    variable, type anotation, temp string, operator

---
+++ 19NOV-LEC3/type.js
@@ -0,0 +1,21 @@
+"use strict";
+//done by sir kamran
+let x = 10;
+console.log(x);
+let y = "This is type annotation lectue in type script";
+console.log("lecture no 3 : ", y);
+let c = true;
+console.log(c);
+let a = "Aina ";
+let b = "Ahmad";
+console.log("My name is :", a + b);
+// By Sir Jahanzaib below
+console.log("this is the lec by sir jahanzaib\n");
+let firstname = "Ayesha ";
+console.log(firstname);
+firstname = "Bilal";
+console.log(firstname);
+//x ="Aina ";
+let fname = "Aina ";
+let lname = " Ahmad";
+console.log(`My First name is ${fname} and last name is ${lname}       ${23}      ${lname} .`);
---
+++ 19NOV-LEC3/type.ts
@@ -0,0 +1,23 @@
+//done by sir kamran
+let x: number = 10;
+console.log(x);
+let y: string = "This is type annotation lectue in type script";
+console.log("lecture no 3 : ", y);
+let c: boolean = true;
+console.log(c);
+let a: string = "Aina ";
+let b: string = "Ahmad";
+console.log("My name is :", a + b);
+
+// By Sir Jahanzaib below
+console.log("this is the lec by sir jahanzaib\n");
+let firstname = "Ayesha ";
+console.log(firstname);
+firstname = "Bilal";
+console.log(firstname);
+//x ="Aina ";
+let fname = "Aina ";
+let lname = " Ahmad";
+console.log(
+  `My First name is ${fname} and last name is ${lname}       ${23}      ${lname} .`
+);
